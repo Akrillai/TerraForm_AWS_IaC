2ef3da4c3c47427016d9eba65a43430165062f91
Akrillai <Marks.Legkovskis@warwick.ac.uk>
2022-10-30 21:44:08 +0000

    var quotes attempt

--- config.tf
+++ config.tf
@@ -85,9 +85,9 @@ resource "aws_instance" "build_instance" {
 sudo apt update && sudo apt install -y openjdk-8-jdk maven awscli
 git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
 cd boxfuse-sample-java-war-hello && mvn package
-export AWS_ACCESS_KEY_ID=${access_key}
-export AWS_SECRET_ACCESS_KEY=${secret_key}
-export AWS_DEFAULT_REGION=${region}
+"export AWS_ACCESS_KEY_ID=${access_key}"
+"export AWS_SECRET_ACCESS_KEY=${secret_key}"
+"export AWS_DEFAULT_REGION=${region}"
 aws s3 cp target/hello-1.0.war s3://mybucket15.test5.com
 EOF
   
@@ -107,9 +107,9 @@ resource "aws_instance" "prod_instance" {
   user_data = <<EOF
 #!/bin/bash
 sudo apt update && sudo apt install -y openjdk-8-jdk tomcat8 awscli
-export AWS_ACCESS_KEY_ID=${access_key}
-export AWS_SECRET_ACCESS_KEY=${secret_key}
-export AWS_DEFAULT_REGION=${region}
+"export AWS_ACCESS_KEY_ID=${access_key}"
+"export AWS_SECRET_ACCESS_KEY=${secret_key}"
+"export AWS_DEFAULT_REGION=${region}"
 aws s3 cp s3://mybucket15.test5.com/hello-1.0.war /tmp/hello-1.0.war
 sudo mv /tmp/hello-1.0.war /var/lib/tomcat8/webapps/hello-1.0.war
 sudo systemctl restart tomcat8
